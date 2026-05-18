d8acbaee77b40f1d36f17b7e3769cf5b72e654f7
yashsarnaik <yashsarnaik2303@gmail.com>
2026-05-18 14:13:14 +0530

    chore: update PM2 ecosystem instances from max to 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "agent-starter-react",
       script: "node_modules/next/dist/bin/next",
       args: "start",
-      instances: "max",
+      instances: 2,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
